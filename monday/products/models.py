95bc0f98c661cf468aec69ae443f6dd8f31cf90d
Duke09 <debopriyo09@outlook.com>
2020-05-18 16:35:32 +0530

    Created Product Model

--- monday/products/models.py
+++ monday/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Prodcut(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
