643a12dba8a4a3f5f87e457a2c09753d342551bf
Duke09 <debopriyo09@outlook.com>
2020-05-31 09:05:54 +0530

    Initial - Customer user model

--- monday/accounts/models.py
+++ monday/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    # full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = [] #['full_name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+    
+    def get_short_name(self):
+        return
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
--- monday/main/settings.py
+++ monday/main/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'orders',
     'address',
     'search',
+    'accounts',
 ]
 
 MIDDLEWARE = [
