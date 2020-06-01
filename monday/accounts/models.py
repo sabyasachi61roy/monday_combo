24c695fef9082f4ef7b1f3c68d75023eb243eba1
Duke09 <debopriyo09@outlook.com>
2020-06-01 15:28:06 +0530

    User manager

--- monday/accounts/models.py
+++ monday/accounts/models.py
@@ -1,7 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=False, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError('User must have an email address')
+        if not password:
+            raise ValueError("Users must have a password")
+        user = self.model(
+            emai = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.active = is_active
+        user.staff = is_staff
+        user.admin = is_admin
+        user.save(using=self._db)
+        return user
+    
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email, password=password, is_staff=True
+        )
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email, password=password, is_staff=True, is_admin=True
+        )
+        return user
+
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True, max_length=255)
     # full_name = models.CharField(max_length=255, blank=True, null=True)
@@ -14,6 +43,8 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = [] #['full_name']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
