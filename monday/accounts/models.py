d6f6786b5c616487ce6800e76057f76e380b0050
Duke09 <debopriyo09@outlook.com>
2020-06-01 16:10:26 +0530

    User Creation module

--- monday/accounts/models.py
+++ monday/accounts/models.py
@@ -53,7 +53,14 @@ class User(AbstractBaseUser):
         return self.email
     
     def get_short_name(self):
-        return
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
 
     @property
     def is_staff(self):
